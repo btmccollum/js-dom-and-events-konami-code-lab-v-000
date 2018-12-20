ad546f25d90a658ff3c2b208717f5d6b6a0ff262
btmccollum <btmcco8@gmail.com>
2018-12-20 09:00:39 -0600

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let i = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  const main = document.body;
+  main.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    // console.log(key);
+    if (key === code[i]) {
+      i++;
+        if (i === code.length) {
+          alert('You did it!');
+        }
+    } else {
+      i = 0;
+    }
+  });
 }
